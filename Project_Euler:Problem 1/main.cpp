13b5e7c49e045a8669945b13911a5dee881b1853
Ryan Wong <snivysteelhci@gmail.com>
2014-04-05 22:35:42 +0800

    Initial Commit

---
+++ Project_Euler:Problem 1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project_Euler:Problem 1
+//
+//  Created by Ryan Wong on 5/4/14.
+//  Copyright (c) 2014 rwjhwong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
